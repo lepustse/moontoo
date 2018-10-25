2dc343ccc63be294252a4793a91e747d16a057bb
neverxie <neverxie@outlook.com>
2018-10-25 11:03:37 +0800

    ...

--- plat/qemu/qemu.c
+++ plat/qemu/qemu.c
@@ -1,4 +1,7 @@
 #include "all.h"
+
+unsigned int asm_sum(unsigned int x, unsigned y);
+
 void main(void) {
 	int i;
 	int sum = 0;
@@ -8,4 +11,7 @@ void main(void) {
 	}
 	printf("1 + 2 + ... + 100 = %d\n", sum);
 	printf("hello world!\n");
+
+	sum = asm_sum(3, 2);
+	printf("3 + 2 = %u\n", sum);
 }
