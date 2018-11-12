8f282787b95e5f103d2741c0ffab7566d5ef1b66
neverxie <neverxie@outlook.com>
2018-11-12 15:34:19 +0800

    [del] all changes

--- plat/qemu/qemu.c
+++ plat/qemu/qemu.c
@@ -1,7 +1,5 @@
 #include "all.h"
 
-unsigned int asm_sum(unsigned int x, unsigned y);
-
 void main(void) {
 	int i;
 	int sum = 0;
@@ -11,16 +9,4 @@ void main(void) {
 	}
 	printf("1 + 2 + ... + 100 = %d\n", sum);
 	printf("hello world!\n");
-
-	sum = asm_sum(3, 2);
-	printf("3 + 2 = %u\n", sum);
-}
-
-// r0
-void show_num(unsigned int num)
-{
-//	printf("num: %d\n", num);
-	//num = 2;
-	printf("num: 0x%x\n", num);
-	//printf("num's addr: 0x%x\n", &num);
 }
