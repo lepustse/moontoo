d8bcf85f980b16bd4000e7d8bc1b3dc8a094347f
neverxie <neverxie@outlook.com>
2018-12-26 19:53:30 +0800

    【增加】调用栈debug

--- plat/qemu/qemu.c
+++ plat/qemu/qemu.c
@@ -1,12 +1,35 @@
 #include "all.h"
 
+void funcA(void);
+void funcB(void);
+void funcC(void);
+
 void main(void) {
 	int i;
 	int sum = 0;
 
-	for (i = 1; i <= 100; i++) {
-		sum += i;
-	}
-	printf("1 + 2 + ... + 100 = %d\n", sum);
-	printf("hello world!\n");
+	//for (i = 1; i <= 100; i++) {
+	//	sum += i;
+	//}
+	//printf("1 + 2 + ... + 100 = %d\n", sum);
+	//printf("hello world!\n");
+
+    funcA();
+}
+
+void funcA(void) {
+    funcB();
+}
+
+void funcB(void) {
+    int j = 1;
+
+    j++;
+    funcC();
+}
+
+void funcC(void) {
+    int i = 1;
+
+    i++;
 }
