6fbdb34eb407123047e63d69e7291e032b2eec94
neverxie <neverxie@outlook.com>
2018-12-27 14:08:25 +0800

    【增加】mt_memcpy API

--- plat/qemu/qemu.c
+++ plat/qemu/qemu.c
@@ -1,5 +1,12 @@
 #include "all.h"
 
+extern void mt_memcpy(void *dst, void *src, unsigned int size);
+
+char src[7] = {1, 2, 3, 4, 5, 6, 7};
+char dst[7];
+
+char s[4] = "tuz\0";
+
 void main(void) {
 	int i;
 	int sum = 0;
@@ -9,4 +16,10 @@ void main(void) {
 	}
 	printf("1 + 2 + ... + 100 = %d\n", sum);
 	printf("hello world!\n");
+
+    mt_memcpy(dst, src, 0);
+    for (i = 0; i < 7; i++) {
+        printf("dst[%d]:%d\n", i, dst[i]);
+    }
+    printf("end\n");
 }
