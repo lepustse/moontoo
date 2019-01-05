353889846eb492a2ae7133ec941c9074d15ff602
neverxie <neverxie@outlook.com>
2019-01-05 12:04:48 +0800

    【增加】mt_memset

--- plat/qemu/qemu.c
+++ plat/qemu/qemu.c
@@ -1,9 +1,11 @@
 #include "all.h"
 
 extern void mt_memcpy(void *dst, void *src, unsigned int size);
+extern void mt_memset(void *d, int c, unsigned int size);
 
-char src[7] = {1, 2, 3, 4, 5, 6, 7};
+char src[7] = {0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7};
 char dst[7];
+unsigned int tuz[7];
 
 char s[4] = "tuz\0";
 
@@ -17,9 +19,16 @@ void main(void) {
 	printf("1 + 2 + ... + 100 = %d\n", sum);
 	printf("hello world!\n");
 
-    mt_memcpy(dst, src, 0);
+    mt_memcpy(dst, src, 7);
     for (i = 0; i < 7; i++) {
-        printf("dst[%d]:%d\n", i, dst[i]);
+        printf("dst[%d]：0x%08x\n", i, dst[i]);
+    }
+
+    printf("\n");
+
+    mt_memset(dst, 3, 4);
+    for (i = 0; i < 7; i++) {
+        printf("dst[%d]：0x%x\n", i, dst[i]);
     }
     printf("end\n");
 }
