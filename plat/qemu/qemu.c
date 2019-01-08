68a183c9e8ca5f520ef5d7dde382d7d8ac1eebb1
neverxie <neverxie@outlook.com>
2019-01-08 21:32:09 +0800

    【增加】汇编实现不定参函数以及汇编调用不定参函数

--- plat/qemu/qemu.c
+++ plat/qemu/qemu.c
@@ -1,5 +1,36 @@
 #include "all.h"
+#include "stdint.h"
+#include "stdarg.h"
 
+void caller_printf_c(void);
+extern void caller_printf_a(void);
+
+void caller_printf_add_c(void);
+extern void caller_printf_add_a(void);
+
+uint32_t add_c(uint32_t i, ...);
+void caller_add_c(void);
+
+extern uint32_t add_a(uint32_t i, ...);
+extern void caller_add_a(void);
+
+// 求和
+uint32_t add_c(uint32_t i, ...) {
+
+}
+
+// 编译器自动
+void caller_printf_c(void) {
+    int buf[5];
+    for (int i = 0; i < 5; i++) {
+        buf[i] = i;
+    }
+    printf("caller by c: %d, %d, %d, %d, %d\n", buf[0], buf[1], buf[2], buf[3], buf[4]);
+}
+
+void fun(void) {
+    add_a(6, 1, 1, 1, 1, 1, 0);
+}
 void main(void) {
 	int i;
 	int sum = 0;
@@ -9,4 +40,10 @@ void main(void) {
 	}
 	printf("1 + 2 + ... + 100 = %d\n", sum);
 	printf("hello world!\n");
+     caller_printf_a();
+     caller_printf_c();
+
+     i = add_a(6, 1, 2, 3, 4, 5, 6);
+     printf("add求和结果：%d\n", i);
+     caller_add_a();
 }
