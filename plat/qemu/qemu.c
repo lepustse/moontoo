90d4d2c0c5fdb204c46745ed9afbd496670775f1
neverxie <neverxie@outlook.com>
2019-01-12 13:31:26 +0800

    【增加】strlen函数，已经优化mt_memcpy&mt_memset

--- plat/qemu/qemu.c
+++ plat/qemu/qemu.c
@@ -2,13 +2,22 @@
 
 extern void mt_memcpy(void *dst, void *src, unsigned int size);
 extern void mt_memset(void *d, int c, unsigned int size);
+unsigned int _strlen(const char *s);
 
 char src[7] = {0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7};
 char dst[7];
 unsigned int tuz[7];
 
 char s[4] = "tuz\0";
+char *str = "never\0";
 
+unsigned int _strlen(const char *s) {
+    char *p = s;
+
+    while (*p++);
+
+    return (p - s - 1);
+}
 void main(void) {
 	int i;
 	int sum = 0;
@@ -30,5 +39,11 @@ void main(void) {
     for (i = 0; i < 7; i++) {
         printf("dst[%d]：0x%x\n", i, dst[i]);
     }
+
+    //i = mt_strlen(str);
+    i = mt_strlen("");
+    printf("字符串长度：%d\n", i);
+
+    i = _strlen(str);
     printf("end\n");
 }
