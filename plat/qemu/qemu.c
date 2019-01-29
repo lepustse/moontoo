486336ccf58480a4a5d488b8dd54baaa2dde1f26
neverxie <neverxie@outlook.com>
2019-01-29 19:19:44 +0800

    【测试】一个题目

--- plat/qemu/qemu.c
+++ plat/qemu/qemu.c
@@ -1,5 +1,18 @@
 #include "all.h"
 
+char *get_mem(void) {
+    char p[] = "hello world";
+    return p;
+}
+
+void test(void) {
+    char *str;
+
+    str = get_mem();
+    printf("%x\n", str);
+    printf("%s\n", str);
+}
+
 void main(void) {
 	int i;
 	int sum = 0;
@@ -8,5 +21,6 @@ void main(void) {
 		sum += i;
 	}
 	printf("1 + 2 + ... + 100 = %d\n", sum);
-	printf("hello world!\n");
+
+    test();
 }
